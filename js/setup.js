d2c6c292dc26729d13af805329bcc79270c9aab7
Sveta <angelinalana84@gmail.com>
2019-06-15 18:27:29 +0300

    Задание: нас орда

---
+++ js/setup.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+.content
+.querySelector('.setup-similar-item');
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var GamePlayer = function (n, c, e) {
+  this.name = n;
+  this.coatColor = c;
+  this.eyesColor = e;
+};
+var wizards = [];
+
+for (i = 0; i < 4; i++) {
+  var nameRand = Math.floor(Math.random() * WIZARD_NAMES.length);
+  var coatColorRand = Math.floor(Math.random() * coatColor.length);
+  var eyesColorRand = Math.floor(Math.random() * eyesColor.length);
+
+  wizards[i] = new GamePlayer(WIZARD_NAMES[nameRand] + ' ' + WIZARD_SURNAMES[nameRand], coatColor[coatColorRand], eyesColor[eyesColorRand]);
+}
+
+var renderWizard = function () {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
